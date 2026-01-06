0f49cff2143c1c4d71059da5c0cf291418bcd6d4
muhyo Din <attariattari549@gmail.com>
2026-01-06 08:46:59 -0700

    feat: add getUserChannelProfile function to retrieve channel details with subscription info

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -285,6 +285,69 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, "User cover image updated successfully", user));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  if (!username?.trim()) {
+    throw new ApiError("Username is required", 400);
+  }
+  const channel = await User.aggregate([
+    { $match: { username: username?.toLowerCase() } },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: { $size: "$subscribers" },
+        subscribedToCount: { $size: "$subscribedTo" },
+        isSubscribed: {
+          $cond: {
+            if: {
+              $in: [req.user?._id, "$subscribers.subscriber"],
+              then: true,
+              else: false,
+            },
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        email: 1,
+        subscribersCount: 1,
+        subscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        createdAt: 1,
+      },
+    },
+  ]);
+  console.log(channel);
+  if (!channel || channel.length === 0) {
+    throw new ApiError("Channel not found", 404);
+  }
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, "Channel profile fetched successfully", channel[0])
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -295,4 +358,5 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
 };
